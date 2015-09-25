c2b4d88053709c4689bfe2f332a5e9e83dca4c35
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2015-09-25 15:08:52 +0100

    "Learn to Program" Chris Pine chapters 1-5

---
+++ Letters1.rb
@@ -0,0 +1,12 @@
+puts 'fuckin\''+" moo "*5
+
+puts '15'.to_f
+puts '99.99'.to_f
+puts '99.99'.to_i
+puts ''
+puts '5 is my fav number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i 
---
+++ TOC.rb
@@ -0,0 +1,20 @@
+
+main_title="Table of Contents"
+ch1_title="Chapter 1:"
+ch1_content="Getting started"
+ch1_page="Page 1"
+
+ch2_title="Chapter 2:"
+ch2_content="Numbers"
+ch2_page="Page 9"
+
+ch3_title="Chapter 3:"
+ch3_content="Letters"
+ch3_page="Page 23"
+
+puts('')
+puts(main_title.center(50))
+puts('')
+puts(ch1_title.ljust(13)+ch1_content.ljust(30)+ch1_page)
+puts(ch2_title.ljust(13)+ch2_content.ljust(30)+ch2_page)
+puts(ch3_title.ljust(13)+ch3_content.ljust(30)+ch3_page)
