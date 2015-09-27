241cd26066776364c509a095625a8bd07976b799
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2015-09-27 12:09:51 +0100

    Chapter 10: Added recursive sortig

--- Array_sorting_words.rb
+++ Array_sorting_words.rb
@@ -1,3 +1,19 @@
+def sort_array some_array
+    sort_recursively some_array, []
+end
+
+def sort_recursively start_array, sorted_array
+    if start_array.length>0
+        sorted_array.push(start_array.min)
+        start_array.delete_at(start_array.index(start_array.min))
+        sort_recursively start_array, sorted_array
+    else
+        return sorted_array
+    end
+end
+
+
+
 words_array=[]
 i=0
 puts 'Enter a number of words into an array or press ENTER to finish.'
@@ -14,5 +30,8 @@ while true
         break
     end
 end
-puts 'All the entered words in alphabetical order:'
+puts 'Array of words sorted with a ".sort" method:'
 puts(words_array.sort)
+puts
+puts 'Array of words sorted with a recursive method:'
+puts(sort_array words_array)
