357736c53198686f1d76d8892ef488311ad28e73
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2015-10-09 11:57:18 +0100

    Codewars: nailing it finally

--- CW_find_seq.rb
+++ CW_find_seq.rb
@@ -1,10 +1,7 @@
 def length_of_sequence(arr,n)
-    if !arr.include? n or arr.length == 1 or arr.count(n)>2
+    if arr.length == 1 or arr.count(n)!=2
       return 0
     else
-      arr[arr.index(n),arr[arr.index(n)+1,arr.length-1].index(n)+arr.index(n)+1].length
+      return 1+arr.rindex(n)-arr.index(n)
     end
 end
-
-puts [1,2,3].length
-puts length_of_sequence([1, 1], 1)
