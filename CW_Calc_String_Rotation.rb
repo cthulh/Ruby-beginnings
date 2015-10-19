0d7e16ac4bc5eae3a1342175ff5386ffb91aab62
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2015-10-19 18:03:12 +0100

    Codewars kata6

--- CW_Calc_String_Rotation.rb
+++ CW_Calc_String_Rotation.rb
@@ -1,20 +1,18 @@
 def shifted_diff(first, second)
 	t = first.split("").length
-	if first.split("").sort == second.split("").sort
+	#return -1 if !first.split("").sort == second.split("").sort
+	return 0 if first==second
 		while t>1
-			if second.include? first[0,t-1]
-				puts first[0,t-1] if t==2
+			if second.include? first[0,t-1] and second[second.index(first[0,t-1]),second.length-1]<<second[0,second.index(first[0,t-1])]==first
 				return second.index(first[0,t-1])
 			else
 				t-=1
 			end
 		end
 		return -1
-	else
-		return -1
-	end
 end
 
 puts shifted_diff("eecoff","coffee")
-puts shifted_diff("isn't","'tisn")
-puts shifted_diff("Moose","ooseM")
+puts shifted_diff("isn't ","'t isn")
+puts shifted_diff("Moo se","Moo se")
+puts shifted_diff("dog","god")
