27f13b2f0adee150565fe93a08ef9849ea8bbcfe
Cthulh <tryglavson@hotmail.co.uk>
2016-01-16 18:39:31 +0000

    Chapter 3 classes

--- Dave Thomas - Programming Ruby/Chapter3.rb
+++ Dave Thomas - Programming Ruby/Chapter3.rb
@@ -1,15 +1,24 @@
 class BookInStock
+  attr_reader :isbn, :price
   def initialize(isbn , price)
     @isbn = isbn
     @price = Float(price)
+  end
+  def to_s
+  	"ISBN: #{@isbn}, price: #{@price}"
   end
 end
 
 b1 = BookInStock.new("isbn1",3)
 puts b1
+puts "ISBN of book 1 is #{b1.isbn}"
+puts "Price of book 1 is #{b1.price}"
 
+puts
 b2 = BookInStock.new("isbn2", 3.14)
 puts b2
 
+
+puts
 b3 = BookInStock.new("isbn3", 5.67)
 puts b3
