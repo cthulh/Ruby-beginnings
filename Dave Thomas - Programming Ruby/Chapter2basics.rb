068eea06872363bca09d4c7583e18f124574f4c0
Cthulh <tryglavson@hotmail.co.uk>
2016-01-16 16:49:29 +0000

    Chapter 2

--- Dave Thomas - Programming Ruby/Chapter2basics.rb
+++ Dave Thomas - Programming Ruby/Chapter2basics.rb
@@ -16,10 +16,29 @@ puts a.inspect
 
 a[1] = nil
 a[2] = 'cat'
-a[3] = "someword".split(//).map!{|i| (i == "a" or i == "o" or i == "e") ? i = "X" : i }.join()
+a[3] = "someword".split(//).map!{|i| (i == "a" or i == "o" or i == "e") ? i = "X" : i }
 
 puts a.inspect
 
 a.flatten!
 
 puts a.inspect
+
+a[0..4] = %w{dog bomb cat guitar singer}
+a[5..10] = [nil, nil]
+puts a.inspect
+
+####################
+2.times do puts end
+inst_section = {
+
+	cello: 'string',
+	clarinet: 'woodwind',
+	drum: 'percussion',
+	oboe: 'woodwind',
+	trumpet: 'brass',
+	violin: 'string'
+}
+
+puts inst_section.inspect
+puts inst_section[:trumpet]
